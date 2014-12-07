4d8688c34571a8c38b5a3a9657078bc9f8062804
zakharovg <zakharov.g@gmail.com>
2014-12-07 21:34:57 +0300

    added canvas to list & templates for items

---
+++ AnimationMaker/Resources/FiguresTemplateSelector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using AnimationMaker.ViewModel;
+
+namespace AnimationMaker.Resources
+{
+	public sealed class FiguresTemplateSelector : DataTemplateSelector
+	{
+		public DataTemplate PointTemplate { get; set; }
+		public DataTemplate EdgeTemplate { get; set; }
+
+		public override DataTemplate SelectTemplate(object item, DependencyObject container)
+		{
+			var point = item as IPointViewModel;
+			if (point != null)
+				return PointTemplate;
+
+			var edge = item as IEdgeViewModel;
+			if (edge != null)
+				return EdgeTemplate;
+
+			throw new InvalidOperationException("Template for item is not defined");
+		}
+	}
+}
